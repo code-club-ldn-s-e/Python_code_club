cef40bc2cb8141986b8ff6ccd188d08d1a17e4b2
emcbride09 <elliot.a.mcbride@gmail.com>
2019-05-18 15:01:42 +0100

    Update eu_fatal.py

--- eu_fatal.py
+++ eu_fatal.py
@@ -1,6 +1,15 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 eu_fat = pd.read_csv("eu_terrorism_fatalities_by_country.csv") 
 
 eu_fat.head()
+
+#print(eu_fat.loc[:, 'France'])
+#print(eu_fat[['France', 'Belgium']])
+print(eu_fat[['iyear','France', 'Belgium']])
+
+fr_bel = eu_fat[['iyear','France', 'Belgium']]
+
+fr_bel.plot(x = 'iyear', y = 'France')
+plt.show()
